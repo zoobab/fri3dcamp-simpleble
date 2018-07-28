d8f39f5d4089bb87823ed59c8bff7ced59fdb75a
Benjamin Henrion (zoobab) <zoobab@gmail.com>
2018-07-29 01:28:36 +0200

    count the number of frieds

---
+++ count-frieds.ino
@@ -0,0 +1,44 @@
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+int scanTime = 30; //In seconds
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  BLEScan* pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  BLEScanResults foundDevices = pBLEScan->start(scanTime);
+  Serial.println("Dumping: ");
+  int counter=0;
+  for (int i=0; i<foundDevices.getCount(); i++) {
+     String blename = foundDevices.getDevice(i).getName().c_str();
+     if ( blename.indexOf("fried") == 0 ){
+         Serial.println(blename);
+         counter++;
+     }
+  }
+  Serial.println("Number of frieds: ");
+  Serial.println(counter);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
