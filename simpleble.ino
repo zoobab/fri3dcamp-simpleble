22f79733e7ff9d8eeeecc324509180083b8312ea
Benjamin Henrion <zoobab@gmail.com>
2020-04-28 23:20:45 +0200

    remove 2chars

--- simpleble.ino
+++ simpleble.ino
@@ -1,26 +1,26 @@
-  // Simple BLE for FriedCamp Badge ESP32
-  // Beacon is friedMACADDR , like "friedAABBCCDDEEFF"
+// Simple BLE for FriedCamp Badge ESP32
+// Beacon is friedMACADDR , like "friedAABBCCDDEEFF"
+
+#include "SimpleBLE.h"
+#include "WiFi.h"
+
+SimpleBLE ble;
   
-  #include "SimpleBLE.h"
-  #include "WiFi.h"
+void setup() {
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+    //  waiting for 5secs, sometimes if I do not wait the MAC is 00:00:00:00:00:00, WTF
+    sleep(5);
+    String mac = WiFi.macAddress();
+    String macwdot = mac;
+    macwdot.replace(":", "");
+    String fried = "fried";
+    String friedmac = fried + macwdot;
+    ble.begin(friedmac);
+    Serial.println(mac);
+    Serial.println(friedmac);
+}
   
-  SimpleBLE ble;
-  
-  void setup() {
-      Serial.begin(115200);
-      Serial.setDebugOutput(true);
-  //  waiting for 5secs, sometimes if I do not wait the MAC is 00:00:00:00:00:00, WTF
-      sleep(5);
-      String mac = WiFi.macAddress();
-      String macwdot = mac;
-      macwdot.replace(":", "");
-      String fried = "fried";
-      String friedmac = fried + macwdot;
-      ble.begin(friedmac);
-      Serial.println(mac);
-      Serial.println(friedmac);
-  }
-  
-  void loop() {
-      while(Serial.available()) Serial.write(Serial.read());
-  }
+void loop() {
+    while(Serial.available()) Serial.write(Serial.read());
+}
