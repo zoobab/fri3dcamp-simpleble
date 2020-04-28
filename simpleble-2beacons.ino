310232fdcba8018c242a0ea060664c8e7b0ebb8d
Benjamin Henrion <zoobab@gmail.com>
2020-04-28 23:37:20 +0200

    2 beacons

--- simpleble-2beacons.ino
+++ simpleble-2beacons.ino
@@ -3,22 +3,26 @@
 
 #include "SimpleBLE.h"
 #include "WiFi.h"
-  
+
 SimpleBLE ble;
   
 void setup() {
     Serial.begin(115200);
     Serial.setDebugOutput(true);
-//  waiting for 5secs, sometimes if I do not wait the MAC is 00:00:00:00:00:00, WTF
+    //  waiting for 5secs, sometimes if I do not wait the MAC is 00:00:00:00:00:00, WTF
     sleep(5);
     String mac = WiFi.macAddress();
     String macwdot = mac;
     macwdot.replace(":", "");
-    String fried = "fried";
-    String friedmac = fried + macwdot;
-    ble.begin(friedmac);
-    Serial.println(mac);
-    Serial.println(friedmac);
+    String head = "covid";
+    String beacon_name = head + macwdot;
+    String beacon_name_2 = head + macwdot + 2;
+    
+    ble.begin(beacon_name);
+    Serial.println(beacon_name);
+    
+    ble.begin(beacon_name_2);
+    Serial.println(beacon_name_2);
 }
   
 void loop() {
